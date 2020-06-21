46cb374ef6fd8946776ee47ec071bc6b9be67965
chen-zhoujian <1536920466@qq.com>
2020-06-21 10:58:05 +0800

    SegNet网络

---
+++ SegNet.py
@@ -0,0 +1,218 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2 as cv
+import numpy as np
+from PIL import Image
+import torch.utils.data as Data
+import os
+import time
+import argparse
+
+
+bn_momentum = 0.1  # BN层的momentum
+torch.cuda.manual_seed(1)  # 设置随机种子
+
+# 编码器
+class Encoder(nn.Module):
+    def __init__(self, input_channels):
+        super(Encoder, self).__init__()
+
+        self.enco1 = nn.Sequential(
+            nn.Conv2d(input_channels, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.enco2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.enco3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.enco4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.enco5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        id = []
+
+        x = self.enco1(x)
+        x, id1 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)  # 保留最大值的位置
+        id.append(id1)
+        x = self.enco2(x)
+        x, id2 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
+        id.append(id2)
+        x = self.enco3(x)
+        x, id3 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
+        id.append(id3)
+        x = self.enco4(x)
+        x, id4 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
+        id.append(id4)
+        x = self.enco5(x)
+        x, id5 = F.max_pool2d(x, kernel_size=2, stride=2, return_indices=True)
+        id.append(id5)
+
+        return x, id
+
+
+# 编码器+解码器
+class SegNet(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super(SegNet, self).__init__()
+
+        self.weights_new = self.state_dict()
+        self.encoder = Encoder(input_channels)
+
+        self.deco1 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.deco2 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.deco3 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.deco4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64, momentum=bn_momentum),
+            nn.ReLU()
+        )
+        self.deco5 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64, momentum=bn_momentum),
+            nn.ReLU(),
+            nn.Conv2d(64, output_channels, kernel_size=3, stride=1, padding=1),
+        )
+
+    def forward(self, x):
+        x, id = self.encoder(x)
+
+        x = F.max_unpool2d(x, id[4], kernel_size=2, stride=2)
+        x = self.deco1(x)
+        x = F.max_unpool2d(x, id[3], kernel_size=2, stride=2)
+        x = self.deco2(x)
+        x = F.max_unpool2d(x, id[2], kernel_size=2, stride=2)
+        x = self.deco3(x)
+        x = F.max_unpool2d(x, id[1], kernel_size=2, stride=2)
+        x = self.deco4(x)
+        x = F.max_unpool2d(x, id[0], kernel_size=2, stride=2)
+        x = self.deco5(x)
+
+        return x
+
+    # 删掉VGG-16后面三个全连接层的权重
+    def load_weights(self, weights_path):
+        weights = torch.load(weights_path)
+        del weights["classifier.0.weight"]
+        del weights["classifier.0.bias"]
+        del weights["classifier.3.weight"]
+        del weights["classifier.3.bias"]
+        del weights["classifier.6.weight"]
+        del weights["classifier.6.bias"]
+
+        names = []
+        for key, value in self.encoder.state_dict().items():
+            if "num_batches_tracked" in key:
+                continue
+            names.append(key)
+
+        for name, dict in zip(names, weights.items()):
+            self.weights_new[name] = dict[1]
+
+        self.encoder.load_state_dict(self.weights_new)
+
+
+class MyDataset(Data.Dataset):
+    def __init__(self, txt_path):
+        super(MyDataset, self).__init__()
+
+        paths = open(txt_path, "r")
+
+        image_label = []
+        for line in paths:
+            line.rstrip("\n")
+            line.lstrip("\n")
+            path = line.split()
+            image_label.append((path[0], path[1]))
+
+        self.image_label = image_label
+
+    def __getitem__(self, item):
+        image, label = self.image_label[item]
+
+        image = cv.imread(image)
+        image = cv.resize(image, (224, 224))
+        image = image/255.0  # 归一化输入
+        image = torch.Tensor(image)
+        image = image.permute(2, 0, 1)  # 将图片的维度转换成网络输入的维度（channel, width, height）
+
+        label = cv.imread(label, 0)
+        label = cv.resize(label, (224, 224))
+        label = torch.Tensor(label)
+
+        return image, label
+
+    def __len__(self):
+        return  len(self.image_label)
